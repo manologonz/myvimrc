6534c40e5701a43aace85c34c965dc97b0183323
manologonz <manologonz203@gmail.com>
2022-01-21 13:12:54 -0600

    UPDATE: telescope update and symbols-update remove due to plugin code change

--- lua/meggalord/telescope.lua
+++ lua/meggalord/telescope.lua
@@ -1,55 +1,60 @@
-local pickers = require("telescope.pickers")
-local finders = require("telescope.finders")
-local previewers = require("telescope.previewers")
-local action_state = require("telescope.actions.state")
-local conf = require("telescope.config").values
-local actions = require("telescope.actions")
+local actions = require('telescope.actions')
+-- local opts = {
+--     highlight_hovered_item = true,
+--     show_guides = true
+-- }
 
-require("telescope").setup {
+-- require('symbols-outline').setup(opts)
+require('telescope').setup {
     defaults = {
-        file_sorter = require("telescope.sorters").get_fzy_sorter,
-        prompt_prefix = " >",
+        file_ignore_patterns = {
+            "node_modules",
+            ".git",
+            "plugged",
+            "undodir",
+            "package-lock"
+        },
+        file_sorter = require('telescope.sorters').get_fzy_sorter,
+        prompt_prefix = ' >',
         color_devicons = true,
 
-        file_previewer = require("telescope.previewers").vim_buffer_cat.new,
-        grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
-        qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
+        file_previewer   = require('telescope.previewers').vim_buffer_cat.new,
+        grep_previewer   = require('telescope.previewers').vim_buffer_vimgrep.new,
+        qflist_previewer = require('telescope.previewers').vim_buffer_qflist.new,
 
         mappings = {
             i = {
                 ["<C-x>"] = false,
                 ["<C-q>"] = actions.send_to_qflist,
             },
-        },
-        file_ignore_patterns = {"node_modules", ".git", "plugged", "undodir"},
+        }
     },
     extensions = {
         fzy_native = {
             override_generic_sorter = false,
             override_file_sorter = true,
-        },
-    },
+        }
+    }
 }
 
 require("telescope").load_extension("git_worktree")
-require("telescope").load_extension("fzy_native")
+require('telescope').load_extension('fzy_native')
 
 local M = {}
 M.search_dotfiles = function()
     require("telescope.builtin").find_files({
         prompt_title = "< VimRC >",
-        cwd = '~/.config/nvim',
-        hidden = true,
+        cwd = "$HOME/.config/nvim",
     })
 end
 
 M.git_branches = function()
     require("telescope.builtin").git_branches({
         attach_mappings = function(_, map)
-            map("i", "<c-d>", actions.git_delete_branch)
-            map("n", "<c-d>", actions.git_delete_branch)
+            map('i', '<c-d>', actions.git_delete_branch)
+            map('n', '<c-d>', actions.git_delete_branch)
             return true
-        end,
+        end
     })
 end
 
